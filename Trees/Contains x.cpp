5b2500aa64fcd327a6f200b6f9bd67b27ba6a37f
Prateek Kalyani <prateek.hhps@gmail.com>
2021-07-18 21:20:31 +0530

    added questions

---
+++ Trees/Contains x.cpp
@@ -0,0 +1,33 @@
+// Following is the given Tree node structure.
+/**************
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    
+    TreeNode(T data) {
+        this->data = data;
+    }
+    
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+***************/
+
+bool isPresent(TreeNode < int > * root, int x) {
+
+    if (root -> data == x)
+        return (true);
+
+    for (int i = 0; i < root -> children.size(); i++) {
+        if (isPresent(root -> children[i], x))
+            return (true);
+    }
+
+    return (false);
+
+}
